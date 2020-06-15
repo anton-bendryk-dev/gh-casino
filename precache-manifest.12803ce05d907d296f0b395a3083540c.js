d732f2b8b452fa3314545010d2a6d90c85fca71e
Anton <antonbendrik1993@gmail.com>
2020-06-15 19:05:52 +0300

    first commit

---
+++ precache-manifest.12803ce05d907d296f0b395a3083540c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb2943f4cd49a648349e66902fa9a31",
+    "url": "/gh-casino/index.html"
+  },
+  {
+    "revision": "04402868a88e070dc403",
+    "url": "/gh-casino/static/css/main.793b085f.chunk.css"
+  },
+  {
+    "revision": "86a0e7eeb94f356e048b",
+    "url": "/gh-casino/static/js/2.eac7276c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-casino/static/js/2.eac7276c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04402868a88e070dc403",
+    "url": "/gh-casino/static/js/main.baf700b3.chunk.js"
+  },
+  {
+    "revision": "d7b22110c5c5d81964b6",
+    "url": "/gh-casino/static/js/runtime-main.01d0db42.js"
+  },
+  {
+    "revision": "4e9c62686996d8a5f4579ed920909ece",
+    "url": "/gh-casino/static/media/Group155.4e9c6268.png"
+  },
+  {
+    "revision": "9f563abf8532ead724f2d6231983b5d4",
+    "url": "/gh-casino/static/media/NotoSansArabic-Regular.9f563abf.ttf"
+  },
+  {
+    "revision": "23c2b7c12053d51a4b95c35149a28a0a",
+    "url": "/gh-casino/static/media/NotoSansArabic-SemiBold.23c2b7c1.ttf"
+  },
+  {
+    "revision": "d2ba003273a57e34a40e748c4bd55005",
+    "url": "/gh-casino/static/media/bg.d2ba0032.png"
+  },
+  {
+    "revision": "6faafd9d34383e9ea1fd2b5d28d2b3c9",
+    "url": "/gh-casino/static/media/bgMobile.6faafd9d.png"
+  },
+  {
+    "revision": "480b9d29f458bc066ac2d43a35792e86",
+    "url": "/gh-casino/static/media/f2.480b9d29.png"
+  },
+  {
+    "revision": "0194ba8325563535fd1285a453230bdd",
+    "url": "/gh-casino/static/media/f3_2.0194ba83.png"
+  },
+  {
+    "revision": "fcc8c324ff53db3c253ed5021bc9deed",
+    "url": "/gh-casino/static/media/f3_3.fcc8c324.png"
+  },
+  {
+    "revision": "406a125b004e513b2a796ec463cfe2c7",
+    "url": "/gh-casino/static/media/f4.406a125b.png"
+  },
+  {
+    "revision": "816e6f3fbee691e72a9a8684692024ea",
+    "url": "/gh-casino/static/media/f4_2.816e6f3f.png"
+  },
+  {
+    "revision": "bc32b7622242da6896eba16923645375",
+    "url": "/gh-casino/static/media/girl_left1.bc32b762.png"
+  },
+  {
+    "revision": "3e3289f30c268228c49052898ac6f769",
+    "url": "/gh-casino/static/media/girl_left11.3e3289f3.png"
+  },
+  {
+    "revision": "d7728e3befae3b86c738db2bda2d5805",
+    "url": "/gh-casino/static/media/girl_left3.d7728e3b.png"
+  },
+  {
+    "revision": "0181a245c4514d96450f3aad3a53f82b",
+    "url": "/gh-casino/static/media/girl_right1.0181a245.png"
+  },
+  {
+    "revision": "225a389acc50df8ba6826a811d38b951",
+    "url": "/gh-casino/static/media/girl_right11.225a389a.png"
+  },
+  {
+    "revision": "849ae8a36a5ccee277d3314483e3635a",
+    "url": "/gh-casino/static/media/girl_right3.849ae8a3.png"
+  },
+  {
+    "revision": "5388de31f7aeaccae631a02a00945cb2",
+    "url": "/gh-casino/static/media/pattern_021.5388de31.png"
+  },
+  {
+    "revision": "048d7028d3553e737666e909cd0aefca",
+    "url": "/gh-casino/static/media/registration.048d7028.png"
+  }
+]);
